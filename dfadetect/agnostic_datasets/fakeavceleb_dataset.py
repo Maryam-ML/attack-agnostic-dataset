6054651ba58da3de103908d6940abbd5ee15d3f0
Maryam-ML <maryamg786786@gmail.com>
2026-06-29 01:03:06 +0500

    .

--- dfadetect/agnostic_datasets/fakeavceleb_dataset.py
+++ dfadetect/agnostic_datasets/fakeavceleb_dataset.py
@@ -8,13 +8,14 @@ from dfadetect.agnostic_datasets.base_dataset import SimpleAudioFakeDataset
 
 FAKEAVCELEB_KFOLD_SPLIT = {
     
-    0: {
-        "train": ['fsgan-wav2lip', 'wav2lip'],
-        "test": ['rtvc'],
-        "val": ['faceswap-wav2lip'],
+    0:2: {
+        "train": ['faceswap-wav2lip', 'fsgan-wav2lip'],
+        "test": ['wav2lip'],
+        "val": ['rtvc'],
         "bonafide_partition": [0.7, 0.15],
-        "seed": 43
+        "seed": 44
     }
+}
 }
 
 
