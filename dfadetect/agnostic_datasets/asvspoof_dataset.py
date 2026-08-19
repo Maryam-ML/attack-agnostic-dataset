414ff5498b8136f2aef4eea1e656ceef9264384a
Maryam-ML <maryamg786786@gmail.com>
2026-08-20 03:29:42 +0500

    .

--- dfadetect/agnostic_datasets/asvspoof_dataset.py
+++ dfadetect/agnostic_datasets/asvspoof_dataset.py
@@ -7,12 +7,12 @@ from dfadetect.agnostic_datasets.base_dataset import SimpleAudioFakeDataset
 
 
 ASVSPOOF_KFOLD_SPLIT = {
-    0: {
-        "train": ['melgan_large', 'waveglow', 'full_band_melgan', 'melgan', 'hifiGAN'],
-        "test": ['multi_band_melgan'],
-        "val": ['parallel_wavegan'],
+    0:{
+        "train": ['A01', 'A02', 'A05', 'A06', 'A07', 'A08', 'A09', 'A10', 'A15', 'A16', 'A17', 'A18', 'A19'],
+        "test": ['A03', 'A11', 'A12'],
+        "val": ['A04', 'A13', 'A14'],
         "bonafide_partition": [0.7, 0.15],
-        "seed": 42
+        "seed": 43
     }
 }
 
