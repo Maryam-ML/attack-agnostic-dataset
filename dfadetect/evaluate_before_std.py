ccd2331dcdb9cd092cf0bba0efeec61f77437981
Maryam-ML <maryamg786786@gmail.com>
2026-08-20 02:03:11 +0500

    .

--- dfadetect/evaluate_before_std.py
+++ dfadetect/evaluate_before_std.py
@@ -27,7 +27,7 @@ from dfadetect.cnn_features import CNNFeaturesSetting
 from dfadetect.models import models
 from dfadetect.trainer import NNDataSetting
 from dfadetect.utils import set_seed
-from experiment_config import feature_kwargs
+#from experiment_config import feature_kwargs
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
